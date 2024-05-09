218760b20f1b3272b329fc01f33d414e8dc29472
Sohelsh <sohelshaikhadam@gmail.com>
2024-05-09 23:46:16 +0530

    Assignment Of ASP.NET

---
+++ Assignment_01/First_Project/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace First_Project
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click_Click(object sender, EventArgs e)
+        {
+            lbl_Output.Visible = true;
+            lbl_Output.Text = "Hello ASP.NET";
+            lbl_Output.ForeColor = System.Drawing.Color.Red;
+        }
+    }
+}
