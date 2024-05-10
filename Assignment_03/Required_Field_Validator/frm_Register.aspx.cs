0e55405ffffca8104ad57fdc4ef92711c8be08dc
Sohelsh <sohelshaikhadam@gmail.com>
2024-05-10 22:26:34 +0530

    Assignment Of ASP.NET

---
+++ Assignment_03/Required_Field_Validator/frm_Register.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Required_Field_Validator
+{
+    public partial class frm_Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if (rb_Female.Checked || rb_Male.Checked)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Your Form Has Been Submitted')</script>");
+
+            }  
+        }
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if(rb_Male.Checked || rb_Female.Checked)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                args.IsValid = false;
+            }
+        }
+
+        protected void CustomValidator2_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            //int No = Convert.ToInt32( args.Value);
+            int No = int.Parse(args.Value);
+
+            if(No % 2 == 0)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                args.IsValid = false;
+            }
+        }
+    }
+}
